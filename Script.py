5dddf0ab23a8a7e889d5df9173175ddcffaaa635
PR0FESS0R-Master <98311387+PR0FESS0R-Master@users.noreply.github.com>
2022-03-12 21:17:21 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,11 +1,5 @@
  
 
-- Eva Maria Supports both url and alert inline buttons.
-
-<b>NOTE:</b>
-1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
-3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
 <code>[Button Text](buttonurl:https://t.me/PR0FESS0R_Master)</code>
