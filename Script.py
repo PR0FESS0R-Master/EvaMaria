892d34e223fb5edf2e3804f30f9456c4af790f0d
PR0FESS0R-Master <98311387+PR0FESS0R-Master@users.noreply.github.com>
2022-03-12 21:19:15 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,31 +4,10 @@
 
 
 
-- Used to connect bot to PM for managing filters 
-- it helps to avoid spamming in groups.
+- 
 
-<b>NOTE:</b>
-1. Only admins can add a connection.
-2. Send <code>/connect</code> for connecting me to ur PM
 
-<b>Commands and Usage:</b>
-• /connect  - <code>connect a particular chat to your PM</code>
-• /disconnect  - <code>disconnect from a chat</code>
-• /connections - <code>list all your connections</code>"""
-    EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
-<b>NOTE:</b>
-these are the extra features of Eva Maria
-
-<b>Commands and Usage:</b>
-• /id - <code>get id of a specified user.</code>
-• /info  - <code>get information about a user.</code>
-• /imdb  - <code>get the film information from IMDb source.</code>
-• /search  - <code>get the film information from various sources.</code>"""
-    ADMIN_TXT = """Help: <b>Admin mods</b>
-
-<b>NOTE:</b>
-This module only works for my admins
 
 ID - 
 
